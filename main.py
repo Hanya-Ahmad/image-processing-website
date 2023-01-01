e7a88af281bf5e3504442aa3490f293eb7dc5403
hanya-ahmad <hanya.taweel01@eng.st.cu.edu.eg>
2023-01-01 03:25:23 +0200

    linked new frontend to server

--- main.py
+++ main.py
@@ -1,29 +1,65 @@
 from flask import Flask, render_template,request, json
 import os
+import numpy as np
 # import functions as fn
 app = Flask(__name__)
-UPLOAD_FOLDER = "./static/images"
+
+UPLOAD_FOLDER = ".\static\images"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 @app.route("/",methods=['GET', 'POST'])
 def index():
+    global_first_path = "..\static\images\santamonica.jpg"
+    global_second_path = "..\static\images\messi.jpg"
+    print("methodddddd: ",request.method)
+    first_crop_array=[]
+    print("globalssss",global_first_path,global_second_path)
 
     if request.method == 'POST':
-        print(request.get_data())
-        if 'file2' in request.files and 'file1' in request.files:
+        try:
+            firstData = request.get_json("firstData")
+            first_crop_array = firstData['firstData']
+            # saved_array = first_crop_array
+            print("firstcroparrayyyyy",first_crop_array)
+        except:
+            pass
+        try:
+            secondData = request.get_json("secondData")
+            second_crop_array = secondData['secondData']
+            # saved_array = first_crop_array
+            print("secondcroparrayyyyy",second_crop_array)
+        except:
+            pass
+        if 'file1' in request.files:
             file1 = request.files['file1']
+            print("FILLELEEEEEE",file1)
+
             first_path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
             file1.save(first_path)
+            global_first_path = first_path
+        else:
+            first_path = global_first_path
+        if 'file2' in request.files:
             file2 = request.files['file2']
             second_path = os.path.join(app.config['UPLOAD_FOLDER'], file2.filename)
             file2.save(second_path)
-            # phase_image,magnitude_image = fn.set_images(first_path,second_path)
-            # phase_image_resized , magnitude_image_resized = fn.resizing_image(phase_image,magnitude_image)
-            return render_template('index.html',path_to_first_pic=file1.filename, path_to_second_pic=file2.filename)
-            
+            # first_image,second_image = fn.set_images(first_path,second_path)
+            # first_image_resized , second_image_resized = fn.resizing_image(first_image,second_image)
+            # formData = request.get_json("formData")
+            # print(formData)
+            global_second_path = second_path
         else:
-            return render_template('index.html', path_to_first_pic="messi.jpg", path_to_second_pic="My project.jpg")
+            second_path = global_second_path
+        return render_template('index.html',path_to_first_pic=first_path, path_to_second_pic=second_path,first_crop_array=first_crop_array)
+            
     else:
-        return render_template('index.html', path_to_first_pic="messi.jpg", path_to_second_pic="My project.jpg")
+        # print("globalssss",global_first_path,global_second_path)
+        first_crop_array = []  
+        first_path = global_first_path
+        second_path = global_second_path
+
+        return render_template('index.html', path_to_first_pic=first_path, path_to_second_pic=second_path,first_crop_array=first_crop_array)
+    
 if __name__ == '__main__':        
+
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,80 +1,117 @@
 
-let phaseImageCropX;
-let phaseImageCropY;
-let phaseImageCropWidth;
-let phaseImageCropHeight;
-let magnitudeImageCropX;
-let magnitudeImageCropY;
-let magnitudeImageCropWidth;
-let magnitudeImageCropHeight;
+let firstImageCropX;
+let firstImageCropY;
+let firstImageCropWidth;
+let firstImageCropHeight;
+let secondImageCropX;
+let secondImageCropY;
+let secondImageCropWidth;
+let secondImageCropHeight;
 
-const phaseImage = document.getElementById('phase_img');
-const magnitudeImage = document.getElementById('magnitude_img')
-const phaseCropper = new Cropper(phaseImage, {
+
+let checkbox1 = document.getElementById("for_img_1");
+let checkbox2 = document.getElementById("for_img_2");
+
+// Add a change event listener to each checkbox
+checkbox1.addEventListener("change", function() {
+    if (checkbox1.checked) {
+        checkbox2.checked = false;
+    }
+    else{
+        checkbox2.checked = true;
+    }
+});
+
+checkbox2.addEventListener("change", function() {
+    if (checkbox2.checked) {
+        checkbox1.checked = false;
+    }
+    else{
+        checkbox1.checked = true; 
+    }
+});
+
+const firstImage = document.getElementById('first_image');
+const secondImage = document.getElementById('second_image')
+const firstCropper = new Cropper(firstImage, {
     aspectRatio:0,
     viewMode:3,
      crop(event){
-        phaseImageCropX = event.detail.x;
-        phaseImageCropY = event.detail.y;
-        phaseImageCropWidth = event.detail.width;
-        phaseImageCropHeight = event.detail.height;  
+        firstImageCropX = event.detail.x;
+        firstImageCropY = event.detail.y;
+        firstImageCropWidth = event.detail.width;
+        firstImageCropHeight = event.detail.height;  
 }});
-const magnitudeCropper = new Cropper(magnitudeImage, {
+const secondCropper = new Cropper(secondImage, {
     aspectRatio:0,
     viewMode:3,
      crop(event){
-        magnitudeImageCropX = event.detail.x;
-        magnitudeImageCropY = event.detail.y;
-        magnitudeImageCropWidth = event.detail.width;
-        magnitudeImageCropHeight = event.detail.height;  
+        secondImageCropX = event.detail.x;
+        secondImageCropY = event.detail.y;
+        secondImageCropWidth = event.detail.width;
+        secondImageCropHeight = event.detail.height;  
 }});
 
 const cropPhaseImgBtn = document.getElementById('cropPhaseImgBtn');
 const cropMagnitudeImgBtn = document.getElementById('cropMagnitudeImgBtn')
 
-
+console.log("heree")
 cropPhaseImgBtn.addEventListener('click',
 function(){
-let croppedPhaseImage = phaseCropper.getCroppedCanvas().toDataURL("image/png");
-document.getElementById('phase_output').src = croppedPhaseImage;
+    console.log(" FIRSTTT CLICKEDDD")
+// let croppedFirstImage = firstCropper.getCroppedCanvas().toDataURL("image/png");
+// document.getElementById('first_output').src = croppedFirstImage;
 
 
 //top left point, bottom left point, top right point, bottom right point
-let phaseCropArray=[[phaseImageCropX , phaseImageCropY],
-[phaseImageCropX , phaseImageCropY+phaseImageCropHeight],
-[phaseImageCropX+phaseImageCropWidth , phaseImageCropY],
-[phaseImageCropX+phaseImageCropWidth , phaseImageCropY+phaseImageCropHeight]
+let firstCropArray=[[firstImageCropX , firstImageCropY],
+[firstImageCropX , firstImageCropY+firstImageCropHeight],
+[firstImageCropX+firstImageCropWidth , firstImageCropY],
+[firstImageCropX+firstImageCropWidth , firstImageCropY+firstImageCropHeight]
 ]
 
-let formData=new FormData()
-formData.append('data', phaseCropArray);
-console.log(phaseCropArray)
+console.log(firstCropArray)
 fetch('/', {
           method: 'POST',
-          body: JSON.stringify({'formData':formData}),
-          contentType: "application/json",
-          dataType: 'json'
-      }).then(function (response) {
-        responseClone = response.clone(); // 2
+          body: JSON.stringify({firstData:firstCropArray}),
+          headers:{
+            'Content-Type': 'application/json',
+          'Accept': 'json'
+      }}).then(function (response) {
+        // responseClone = response.clone(); // 2
         return response.json();
-    }).then(function (data) {
-        // Do something with data
-    }, function (rejectionReason) { // 3
-        console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
-        responseClone.text() // 5
-        .then(function (bodyText) {
-            console.log('Received the following instead of valid JSON:', bodyText); // 6
-        });
+    })
 
 });
 
 
-})
 
 
 cropMagnitudeImgBtn.addEventListener('click',
 function(){
-let croppedMagnitudeImage = magnitudeCropper.getCroppedCanvas().toDataURL("image/png");
-document.getElementById('magnitude_output').src = croppedMagnitudeImage;
+    console.log("SECOND CLICKEDDD")
+// let croppedSecondImage = secondCropper.getCroppedCanvas().toDataURL("image/png");
+// document.getElementById('second_output').src = croppedSecondImage;
+
+
+
+//top left point, bottom left point, top right point, bottom right point
+let secondCropArray=[[secondImageCropX , secondImageCropY],
+[secondImageCropX , secondImageCropY+secondImageCropHeight],
+[secondImageCropX+secondImageCropWidth , secondImageCropY],
+[secondImageCropX+secondImageCropWidth , secondImageCropY+secondImageCropHeight]
+]
+
+fetch('/', {
+    method: 'POST',
+    body: JSON.stringify({secondData:secondCropArray}),
+    headers:{
+      'Content-Type': 'application/json',
+    'Accept': 'json'
+}}).then(function (response) {
+
+  return response.json();
+})
 
 });
+
